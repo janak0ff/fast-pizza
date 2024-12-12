8754fd26725f1a18deb95320aa775b7cceb5e608
Janak shrestha <janak0ff@gmail.com>
2024-12-12 17:35:17 +0545

    A New Way Of Implementing Routes

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,4 +1,4 @@
-import { formatCurrency } from '../../utils/helpers';
+import { formatCurrency } from "../../utilities/helpers";
 
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
