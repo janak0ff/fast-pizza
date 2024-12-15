869265ab65f27de1b681d86f28c8d9f910336ce1
Janak kr shrestha <janakkrshrestha@gmail.com>
2024-12-15 18:17:04 +0545

    Styling Text

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/cart/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
+    <header className="bg-yellow-500 tracking-widest">
       <Link to="/">Fast React Pizza Co.</Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 }
