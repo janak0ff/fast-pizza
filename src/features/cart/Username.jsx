c3fb7391d5341fec298eb87c46cf77fca2dca4bc
Janak kr shrestha <janakkrshrestha@gmail.com>
2024-12-15 19:10:34 +0545

    The Box Model Spacing, Borders, and Display

--- src/features/cart/Username.jsx
+++
@@ -1,5 +0,0 @@
-function Username() {
-  return <div className="text-sm font-semibold">Janak</div>;
-}
-
-export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
-import Username from "../features/cart/Username";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-500 tracking-widest">
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="border-b border-stone-300 bg-yellow-500 px-4 py-3 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
       <Username />
     </header>
