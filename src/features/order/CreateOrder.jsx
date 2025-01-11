75545f8fbdb8b9636239a9d4fc79ae99596db010
Janak kr Shrestha <janakkrshrestha@gmail.com>
2025-01-11 22:10:24 +0545

    Redux Thunks With createAsyncThunk

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,12 +1,13 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice";
 import EmptyCart from "../cart/EmptyCart";
 import store from "../../store";
 import { formatCurrency } from "../../utils/helpers";
 import { useState } from "react";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -77,6 +78,8 @@ function CreateOrder() {
   // message next to the relevant field.
   const formErrors = useActionData();
 
+  const dispatch = useDispatch();
+
   const [withPriority, setWithPriority] = useState(false);
   const cart = useSelector(getCart);
   const totalCartPrice = useSelector(getTotalCartPrice);
@@ -87,6 +90,8 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
+
+<button onClick={()=> dispatch(fetchAddress())}>Get Address</button>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,8 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getAddress } from "../../services/apiGeocoding";
+
+function getPosition() {
+  // Return a new promise that resolves with the user's current geographical position
+  return new Promise(function (resolve, reject) {
+    // Use the browser's geolocation API to get the current position
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+export const fetchAddress = createAsyncThunk("user/fetchAddress", async () => {
+  // Call getPosition to get the current geographical position of the user
+  const positionObj = await getPosition();
+  // Extract latitude and longitude from the position object
+  const position = {
+    latitude: positionObj.coords.latitude,
+    longitude: positionObj.coords.longitude,
+  };
+
+  // Use a reverse geocoding API to convert the position into a human-readable address
+  const addressObj = await getAddress(position);
+  // Format the address components into a single string
+  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+  // Return an object containing both the geographical position and the formatted address
+  return { position, address };
+});
 
 // Define the initial state of the user slice
 const initialState = {
   username: "", // Initially, the userName is an empty string
+  status: "idle", // Initially, the status is set to "idle"
+  position: {},
+  address: "",
+  Error: "",
 };
 
 // Create a slice for user-related state and actions
@@ -19,6 +50,23 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  // Define extra reducers for handling the fetchAddress async thunk
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        // Set the status to "succeeded" and update the position and address in the state
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+        state.state = 'idle'; // Reset the status to "idle" after fetching the address
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        // Set the status to "failed" and save the error message in the state
+        state.status = "failed";
+        state.error = action.error.message;
+      }),
 });
 
 // Export the updateName action creator
@@ -26,30 +74,3 @@ export const { updateName } = userSlice.actions;
 
 // Export the reducer function to be used in the store
 export default userSlice.reducer;
-
-/*function getPosition() {
-  // Return a new promise that resolves with the user's current geographical position
-  return new Promise(function (resolve, reject) {
-    // Use the browser's geolocation API to get the current position
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-}
-
-async function fetchAddress() {
-  // Call getPosition to get the current geographical position of the user
-  const positionObj = await getPosition();
-  // Extract latitude and longitude from the position object
-  const position = {
-    latitude: positionObj.coords.latitude,
-    longitude: positionObj.coords.longitude,
-  };
-
-  // Use a reverse geocoding API to convert the position into a human-readable address
-  const addressObj = await getAddress(position);
-  // Format the address components into a single string
-  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
-
-  // Return an object containing both the geographical position and the formatted address
-  return { position, address };
-}
-*/
