c2f05a64185bb75554278ffebc4b58c301838625
Janak kr Shrestha <janakkrshrestha@gmail.com>
2025-01-14 03:30:43 +0545

    Enhance comments in apiGeocoding.js for improved clarity on function behavior and API interactions

--- src/services/apiGeocoding.js
+++ src/services/apiGeocoding.js
@@ -1,14 +1,17 @@
 // This function takes an object with latitude and longitude properties
 // and returns a promise that resolves with the address for that position.
 export async function getAddress({ latitude, longitude }) {
-  // We make a GET request to the API with the latitude and longitude
+  // Make a GET request to the API with the latitude and longitude
   const res = await fetch(
     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`,
   );
-  // We check if the response was successful. If it wasn't, we throw an error.
+
+  // Check if the response was successful. If it wasn't, throw an error.
   if (!res.ok) throw Error("Failed getting address");
-  // We convert the response to JSON and wait for it to resolve.
+
+  // Convert the response to JSON and wait for it to resolve.
   const data = await res.json();
-  // We return the address data.
+
+  // Return the address data.
   return data;
 }
