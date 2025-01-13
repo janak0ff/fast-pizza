e33a43f8f0296bf88c5be9fffea55547e0b1bc32
Janak kr Shrestha <janakkrshrestha@gmail.com>
2025-01-14 03:23:51 +0545

    Enhance comments in CartItem.jsx for improved clarity on component imports and functionality

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,12 +1,19 @@
+// Import DeleteItem component for deleting an item from the cart
 import DeleteItem from "./DeleteItem";
+// Import formatCurrency function to format prices
 import { formatCurrency } from "../../utils/helpers";
+// Import UpdateItemQuantity component for updating the quantity of an item
 import UpdateItemQuantity from "./UpdateItemQuantity";
+// Import getCurrentQuantityById selector to get the current quantity of an item by its ID
 import { getCurrentQuantityById } from "./cartSlice";
+// Import useSelector hook from react-redux to access the Redux store state
 import { useSelector } from "react-redux";
 
 function CartItem({ item }) {
+  // Destructure item properties for easier access
   const { pizzaId, name, quantity, totalPrice } = item;
 
+  // Get the current quantity of the item from the Redux store
   const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
 
   return (
