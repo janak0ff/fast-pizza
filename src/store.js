99eb16e00d5ce10f94ec1dc65df69d2bf4b6840f
Janak kr Shrestha <janakkrshrestha@gmail.com>
2025-01-14 03:18:50 +0545

    Improve comments in store.js for better clarity on Redux store setup and reducers

--- src/store.js
+++ src/store.js
@@ -1,17 +1,28 @@
 // Import the configureStore function from the @reduxjs/toolkit package
+// This function helps set up a Redux store with good defaults
 import { configureStore } from "@reduxjs/toolkit";
+
 // Import the userReducer from the userSlice.js file
+// This reducer will manage the state related to the user
 import userReducer from "./features/user/userSlice";
+
+// Import the cartReducer from the cartSlice.js file
+// This reducer will manage the state related to the shopping cart
 import cartReducer from "./features/cart/cartSlice";
 
-// Create a store using the configureStore function
+// Create a Redux store using the configureStore function
+// The store holds the complete state tree of the application
 const store = configureStore({
   // Define the reducers that will be used in the store
+  // Each reducer is responsible for updating a specific part of the state
   reducer: {
+    // The user state will be managed by userReducer
     user: userReducer,
+    // The cart state will be managed by cartReducer
     cart: cartReducer,
   },
 });
 
 // Export the store as the default export of the module
+// This allows other parts of the application to access the store
 export default store;
