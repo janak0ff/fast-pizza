2faa3fc65f0c5d36d2465b10902ce76546462997
Janak kr Shrestha <janakkrshrestha@gmail.com>
2025-01-14 03:22:32 +0545

    Enhance comments in Cart.jsx for improved clarity on component functionality and Redux interactions

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,19 +1,29 @@
+// Import Link component from react-router-dom for navigation
 import { Link } from "react-router-dom";
+// Import custom LinkButton component for styled navigation buttons
 import LinkButton from "../../ui/LinkButton";
+// Import custom Button component for styled buttons
 import Button from "../../ui/Button";
+// Import CartItem component to display individual items in the cart
 import CartItem from "./CartItem";
+// Import useDispatch and useSelector hooks from react-redux for interacting with the Redux store
 import { useDispatch, useSelector } from "react-redux";
+// Import clearCart action and getCart selector from cartSlice
 import { clearCart, getCart } from "./cartSlice";
+// Import EmptyCart component to display when the cart is empty
 import EmptyCart from "./EmptyCart";
 
 function Cart() {
+  // Initialize dispatch function to dispatch actions to the Redux store
   const dispatch = useDispatch();
-  // Get the username from Redux store using useSelector
+  // Get the username from the Redux store using useSelector
   const username = useSelector((state) => state.user.username);
-
+  // Get the cart items from the Redux store using the getCart selector
   const cart = useSelector(getCart);
 
+  // If the cart is empty, display the EmptyCart component
   if (!cart.length) return <EmptyCart />;
+
   return (
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
@@ -22,6 +32,7 @@ function Cart() {
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
+          // Render a CartItem component for each item in the cart
           <CartItem item={item} key={item.pizzaId} />
         ))}
       </ul>
@@ -30,8 +41,7 @@ function Cart() {
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-
-        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+        <Button onClick={() => dispatch(clearCart())} type="secondary">
           Clear cart
         </Button>
       </div>
